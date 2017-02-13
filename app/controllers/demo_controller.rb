20d192c60d6192552e430809a4e8c7d0d6ef5184
Chris Lawrence <chris.lawrence@gmx.com>
2017-02-13 11:36:21 +0000

    demo routes and syntax notes added

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,5 +4,23 @@ class DemoController < ApplicationController
 
   # a method in a controller is refered to as an 'Action'
   def index
+    # render(template => 'demo/index') #verbose
+    # render('demo/hello') #will look in 'demo' folder by convention
+    render('index') #default template for blank action
   end
+
+  def hello
+    render('hello')
+  end
+
+  def other_hello
+    # redirect_to(:controller => 'demo', :action => 'index')
+    redirect_to(:action => 'index')
+  end
+
+  def lynda
+    #redirect can be to external url
+    redirect_to('http://lynda.com')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
+
+  # default root route
+  root 'demo#index'
+
   get 'demo/index'
+  get 'demo/hello'
+  get 'demo/other_hello'
+  get 'demo/lynda'
+
+  # 'default route' may be depreciated
+  # get ':controller(/:action(/:id))'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
