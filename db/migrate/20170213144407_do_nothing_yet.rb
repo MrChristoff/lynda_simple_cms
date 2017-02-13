8c2e35d664c171fb9ee8d8b062f076b944d8ee75
Chris Lawrence <chris.lawrence@gmx.com>
2017-02-13 15:52:12 +0000

    db migration for user tabe created and migrated

---
+++ db/migrate/20170213144407_do_nothing_yet.rb
@@ -0,0 +1,16 @@
+class DoNothingYet < ActiveRecord::Migration[5.0]
+  # shorthand for having an 'up' and 'down' method
+  # def change
+  # end
+
+  #changes we want to make to current db
+  def up
+
+  end
+
+  # The reverse of the cahnges we made in 'up' method, eg 'create db' in up
+  # will have a matching 'drop' in down
+  def down
+
+  end
+end
